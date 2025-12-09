917c67112cf6ea3184876cfb4e8dc05e0f4f7c62
Kishorem6002 <kishore.m2024it@sece.ac.in>
2025-12-09 15:03:06 +0530

    Add files via upload

---
+++ Day6/reactDemo/demo1/src/summa2.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react"
+
+
+// without setIsNew
+
+// function Display1()
+// {
+//     const [name, setName] = useState("Kishore")
+//     console.log("Display 1 : my name is",{name})
+//     function ChangeName(Name)
+//     {
+//         console.log("Display 1 : my new name is", {Name})
+//         setName(Name)
+//     }
+
+//     return(
+//         <>
+//             <h1>Display 1: my old name is {name}</h1>
+//             <button onClick={()=>{ChangeName('Kishore M')}}>Click Here to change name !!</button>
+//         </>
+//     )
+// }
+
+// export default Display1
+
+
+//with setNew
+
+function Display1()
+{
+    const [name, setName] = useState("Kishore")
+    const [New,setNew] = useState(false);
+    console.log("Display 1 : my name is",{name})
+    function ChangeName(Name)
+    {
+        console.log("Display 1 : my new name is", {Name})
+        setName(Name)
+        setNew(true)
+    }
+
+    return(
+        <>
+            <h1> Display 1: {New ? "my new name is" : "my old name is"} {name}</h1>
+            <button onClick={()=>{ChangeName('Kishore M')}}>Click Here to change name !!</button>
+        </>
+    )
+}
+
+export default Display1
